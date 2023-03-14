ae99a9fae0baaeb9e405fda695fbc7b1bf213831
Mark Vander Stel <mvndrstl@gmail.com>
2023-03-14 01:20:03 -0400

    WIP rhai version

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,18 @@
-use rustpython_vm as vm;
-use vm::builtins::PyStrRef;
-
-pub(crate) use data_module::make_module;
-
-fn main() {
-    vm::Interpreter::with_init(Default::default(), |vm| {
-        vm.add_native_module("data".to_owned(), Box::new(make_module));
-    })
-    .enter(run);
+fn get_hostname() -> rhai::ImmutableString {
+    "jaguar".into()
 }
 
-fn run(vm: &vm::VirtualMachine) {
-    match exec_module(vm, "prompt") {
-        Ok(prompt) => println!("prompt: '{prompt}'"),
-        Err(exc) => vm.print_exception(exc),
-    };
-}
+pub fn main() -> Result<(), Box<rhai::EvalAltResult>> {
+    let mut engine = rhai::Engine::new();
+    engine.register_fn("get_hostname", get_hostname);
 
-fn exec_module(vm: &vm::VirtualMachine, module: &str) -> vm::PyResult<PyStrRef> {
-    vm.insert_sys_path(vm.new_pyobj("src")).expect("add path");
-    let module = vm.import(module, None, 0)?;
-    let name_func = module.get_attr("generate_prompt", vm)?;
-    let data = data_module::Data { hostname: "jaguar" };
-    let result = vm.invoke(&name_func, (data,))?;
-    let result: PyStrRef = result.try_into_value(vm)?;
+    let ast = engine.compile_file("src/prompt.rhai".into())?;
 
-    vm::PyResult::Ok(result)
-}
+    let mut scope = rhai::Scope::new();
 
-#[vm::pymodule]
-mod data_module {
-    use indexmap::IndexMap;
-    use rustpython_vm::convert::ToPyObject;
-    use rustpython_vm::{
-        function::{FuncArgs, IntoFuncArgs},
-        pyclass, PyResult, VirtualMachine,
-    };
+    let result = engine.call_fn::<String>(&mut scope, &ast, "generate_prompt", ())?;
 
-    #[pyattr]
-    #[pyclass(module = "data_module", name = "Data")]
-    pub struct Data {
-        hostname: &str,
-    }
+    println!("result: '{}'", result);
 
-    #[pyclass]
-    impl Data {
-        #[pymethod]
-        fn hostname(&self) -> PyResult<&str> {
-            Ok(self.hostname)
-        }
-    }
-
-    impl IntoFuncArgs for Data {
-        fn into_args(self, vm: &VirtualMachine) -> FuncArgs {
-            let mut data = IndexMap::new();
-            data.insert("hostname", self.hostname.to_pyobject(vm));
-            let args = vec![data.to_pyobject()];
-            FuncArgs {
-                args: args,
-                kwargs: IndexMap::new(),
-            }
-        }
-    }
+    Ok(())
 }
